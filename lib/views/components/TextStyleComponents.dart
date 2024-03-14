927737c098215c5ac54eb1a8c4bcdbbd72a9731d
omdhameliya <omdhameliya54@gmail.com>
2024-03-14 11:43:09 +0530

    resume builder

---
+++ lib/views/components/TextStyleComponents.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:resume_builder_app/views/utils/ColorUtils.dart';
+
+TextStyle Heading = TextStyle(
+color: BlueColor,
+fontWeight: FontWeight.bold,
+fontSize: 20);
